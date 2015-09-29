3fd7b13c2a5f35c08a4177744c65d4c0c6fb4af1
ReneZwanenburg <renezwanenburg@gmail.com>
2015-09-29 22:41:29 +0200

    Keywords are not Symbols, support NULL literal

--- source/app.d
+++ source/app.d
@@ -4,23 +4,24 @@ enum ConstraintPeg =
 `
 Constraint:
 	Terms			<- Term eoi
-	Term			<- Arithmatic (Comparison / "IS NULL" / "NOT IS NULL" / "IS NOT NULL") (LogicOp Term)?
-	Comparison		<  CompareOp Arithmatic
+	Term			<- Arithmetic (Comparison / "IS NULL" / "NOT IS NULL" / "IS NOT NULL") (LogicOp Term)?
+	Comparison		<  CompareOp Arithmetic
 	CompareOp		<- "<>" / "!=" / '=' / '<' / '>'
 	LogicOp			<  "AND" / "OR"
-	Arithmatic		<- Primary (ArithmaticOp Arithmatic)?
+	Arithmetic		<- Primary (ArithmeticOp Arithmetic)?
 	Primary			<  Parens / Literal / Attribute / Parameter / FuncCall
-	Parens			<- "(" (Term / Arithmatic) ")"
+	Parens			<- "(" (Term / Arithmetic) ")"
 	Attribute		<- Symbol '.' Symbol
 	Parameter		<- '@' Symbol
 	FuncCall		<- Symbol "(" FuncArgs? ")"
-	FuncArgs		<- Arithmatic ("," Arithmatic)*
-	Symbol			<- identifier
-	Literal			<  Number / String
+	FuncArgs		<- Arithmetic ("," Arithmetic)*
+	Symbol			<- !Keyword identifier
+	Literal			<  Number / String / "NULL"
 	String			<~ "'" Char* "'"
 	Char			<- "''" / (!"'" .)
 	Number			<~ [0-9]+ ('.' [0-9]+)?
-	ArithmaticOp	<  '+' / '-' / '/' / '*'
+	ArithmeticOp	<  '+' / '-' / '/' / '*'
+	Keyword			<- "TRUE" / "FALSE" / "NULL" / "IS" / "AND" / "OR"
 `;
 
 mixin(grammar(ConstraintPeg));
