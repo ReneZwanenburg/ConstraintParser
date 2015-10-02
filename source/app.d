400706fe5a7f20caea058acbedf5d0e510ba2c76
ReneZwanenburg <renezwanenburg@gmail.com>
2015-10-02 17:37:49 +0200

    rewrite

--- source/app.d
+++ source/app.d
@@ -5,37 +5,50 @@ enum ConstraintPeg =
 Constraint:
 	Root			<  Expression eoi
 	Expression		<  Equality (And / Or)*
-	Equality		<  Term Equals?
-	Equals			<  ("<>" / "!=" / "<" / ">" / "=") Expression
+	Equality		<  Not? Exists? Term Equals?
+	Equals			<  ("<>" / "!=" / "<=" / ">=" / "<" / ">" / "=" / ;Is / ;Like) Term
+	Is				<~ [Ii][Ss]
+	Like			<~ [Ll][Ii][Kk][Ee]
+	Not				<~ [Nn][Oo][Tt]
+	Exists			<~ [Ee][Xx][Ii][Ss][Tt][Ss]
 	Term			<  Factor (Add / Sub)*
 	Add				<  "+" Factor
 	Sub				<  "-" Factor
-	And				<  ([Aa][Nn][Dd]) Equality
-	Or				<  ([Oo][Rr]) Equality
+	And				<  ~([Aa][Nn][Dd]) Equality
+	Or				<  ~([Oo][Rr]) Equality
 	Factor			<  Primary (Mul / Div)*
 	Mul				<  "*" Primary
 	Div				<  "/" Primary
-	Primary			<  Parens / Neg / FuncCall / Constant
+	Primary			<  Parens / Neg / Cast / FuncCall / Constant
 	Parens			<  "(" Expression ")"
 	Neg				<  "-" Primary
-	FuncCall		<- Symbol "(" FuncArgs? ")"
+	Cast			<- [Cc][Aa][Ss][Tt] "(" Primary [Aa][Ss] DataType ")"
+	DataType		<  ("varchar(" Number ")")
+	FuncCall		<- Symbol ("." Symbol)? "(" FuncArgs? ")"
 	FuncArgs		<  Expression (',' Expression)*
-	Constant		<  Parameter / Attribute / Number / String
-	Parameter		<  "@" Symbol
-	Attribute		<- Symbol "." Symbol
-	Number			<~ [1-9] [0-9]* ('.' [0-9]+)?
+	Constant		<  Parameter / Attribute / Number / String / Null / True / False / Symbol
+	Parameter		<~ "@" Symbol
+	Attribute		<- (QueryNode / Symbol) "." Symbol
+	Number			<~ [0-9]+ ('.' [0-9]+)?
 	String			<~ "'" Char* "'"
 	Char			<- "''" / (!"'" .)
+	QueryNode		<~ '[' Hex4Byte '-' Hex2Byte '-' Hex2Byte '-' Hex2Byte '-' Hex6Byte ']'
+	Hex6Byte		<~ Hex4Byte Hex2Byte
+	Hex4Byte		<~ Hex2Byte Hex2Byte
+	Hex2Byte		<~ HexByte HexByte
+	HexByte			<~ HexDigit HexDigit
+	HexDigit		<- [0-9a-fA-F]
 	Symbol			<  identifier
+	Null			<~ [Nn][Uu][Ll][Ll]
+	True			<~ [Tt][Rr][Uu][Ee]
+	False			<~ [Ff][Aa][Ll][Ss][Ee]
 `;
 
 mixin(grammar(ConstraintPeg));
 
 void main(string[] args)
 {
-	auto inputs = args[1..$];
-
-	foreach(input; inputs)
+	foreach(input; args[1..$])
 	{
 		writeln('='.repeat(40));
 		writeln("Input: ", input);
